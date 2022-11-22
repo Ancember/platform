1b37384ddb04a0e2e410f57362bbee7f8d695e9a
Andrey.Derevyankin <a.derevyankin@atomtech.dev>
2022-11-22 17:34:41 +0300

    Добавлен файл App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let hello = "Hello"
+    export let world = "world"
+</script>
+<h1>
+    {hello}, 
+    <span class="world">
+        {world}
+    </span>
+    !
+</h1>
+<p>
+    123 здесь текст
+</p>
+<style>
+    .world{color: red;}
+</style>
