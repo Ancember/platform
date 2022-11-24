f3b05d440aad559b6f9a5d15f55d6b1b2c4bc805
Andrey Derevyankin <a.derevyankin@atomtech.dev>
2022-11-24 13:54:51 +0300

    тест раскомент и отправка

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import App from './App.svelte'
 const target = document.getElementById('app');
 
 const props = {
-  // hello: 'Привет',
-  // world: 'мир'
+  hello: 'Привет',
+  world: 'мир'
 }
 
 new App({target, props})
